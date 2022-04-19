82185dbe7c377134fade8cea27b58596091a0772
Robert Dennis <robertdennispersonal@gmail.com>
2022-04-19 03:51:38 -0400

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,76 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin')
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette')
+
+const variableColor = (variable) => {
+  const SCALE = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+  const colors = {}
+
+  SCALE.forEach(scale => {
+    colors[scale] = ({ opacityVariable, opacityValue }) => {
+      if (opacityValue !== null) {
+        return `rgba(var(--${variable}-${scale}), ${opacityValue ?? '1'})`
+      }
+
+      if (opacityVariable !== null) {
+        return `rgba(var(--${variable}-${scale}), ${opacityVariable ?? '1'})`
+      }
+
+      return `rgb(var(--${variable}-${scale}))`
+    }
+  })
+
+  return colors
+}
+
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,astro,vue,ts}",
+    "./public/**/*.{html,js,astro,vue,ts}",
+    "./dist/**/*.{html,js,astro,vue,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neutral: variableColor('neutral'),
+        primary: variableColor('primary'),
+        danger: variableColor('danger'),
+        success: variableColor('primary'),
+        warning: variableColor('danger'),
+        gray: variableColor('neutral'),
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    plugin(function ({ addUtilities, matchUtilities, theme }) {
+      addUtilities({
+        '.bg-overlay': {
+          'background': 'linear-gradient(var(--overlay-angle, 90deg), var(--overlay-colors)), var(--overlay-image)',
+          'background-position': 'center',
+          'background-size': 'cover',
+        },
+        '.bg-overlay-colors-dark': {
+          '--overlay-colors': 'rgba(var(--neutral-900),0.9) 60%, rgba(var(--neutral-900),0.7)'
+        },
+        '.bg-gradient-radial': {
+          'background-image': 'radial-gradient(var(--tw-gradient-stops))'
+        },
+      });
+    }),
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          highlight: (value) => ({ boxShadow: `inset 0 1px 0 0 ${value}` }),
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+      )
+    },
+  ],
+  experimental: {
+    optimizeUniversalDefaults: true
+  }
+}
